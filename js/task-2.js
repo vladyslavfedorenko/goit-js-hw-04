3ed3b98c90ecb12182014d773c2c695e452f48ea
vladyslav_fedorenko <godforgiveme1993@gmail.com>
2025-06-01 21:47:26 +0300

    changed

--- js/task-2.js
+++ js/task-2.js
@@ -1,8 +1,12 @@
 function calcAverageCalories(days) {
+  if (days.length === 0) {
+    return 0;
+  }
   let totalCalories = 0;
   for (const day of days) {
     totalCalories += day.calories;
   }
+
   return totalCalories / days.length;
 }
 console.log(
